2b2eb670dfebdb8a95c97fd92690a9beb33e6c3f
suzyahyah <suzyahyah@gmail.com>
2020-05-07 23:40:11 -0400

    refac fns, rename upfront

--- bin/prep_clef.sh
+++ bin/prep_clef.sh
@@ -1,62 +1,80 @@
 #!/usr/bin/env bash
 # Author: Suzanna Sia
 
-# 1. Copy from Kevin's dir
+source ./bin/utils.sh
+
 sstage=1
 estage=1
 
-processd=(mapping)
+processd=(doc rel)
 
 DIR0003_org=/home/hltcoe/kduh/data/ir/clef00-03
 DIR0304_org=/home/hltcoe/kduh/data/ir/clef03-04
 
 DIR0003=/home/hltcoe/ssia/clef00-03
 DIR0304=/home/hltcoe/ssia/clef03-04
-#DIR0003=/export/c12/ssia/data/clef00-03
-#DIR0304=/export/c12/ssia/data/clef03-04
 
 DOCS=$DIR0003/DocumentData/DataCollections
+DOCS_org=$DIR0003_org/DocumentData/DataCollections
+
 RELS=$DIR0003/RelAssess
+QUERIES=$DIR0003/Topics
 
 declare -A L
+declare -A L_org
 
 L=(
 #['english']=${DOCS}/English_data
 #['french']=${DOCS}/French_data
 #['german']=${DOCS}/German_data
-#['russian']=${DOCS}/Russian_data
-['spanish']=${DOCS}/Spanish_data
+['russian']=${DOCS}/Russian_data
+#['spanish']=${DOCS}/Spanish_data
 )
 
-conv_lang() {
-  [[ "$1" == "english" ]] && lang="EN";
-  [[ "$1" == "spanish" ]] && lang="ES";
-  [[ "$1" == "german" ]] && lang="DE";
-  [[ "$1" == "french" ]] && lang="FR";
-  [[ "$1" == "finnish" ]] && lang="FI";
-  [[ "$1" == "italian" ]] && lang="IT";
-  [[ "$1" == "russian" ]] && lang="RU";
-  [[ "$1" == "swedish" ]] && lang="SV";
-  echo "$lang"
-}
+L_org=(
+#['english']=${DOCS_org}/English_data
+#['french']=${DOCS_org}/French_data
+#['german']=${DOCS_org}/German_data
+['russian']=${DOCS_org}/Russian_data
+#['spanish']=${DOCS_org}/Spanish_data
+)
 
 
-
-rm_mk(){
-  [ -d $1 ] && rm -r $1
-  mkdir -p $1
-}
 
 # Stage 0: Get Data
-#if [ $sstage -le 0 ] && [ $estage -ge 0 ]; then
-  # removing and copying directories
+if [ $sstage -le 0 ] && [ $estage -ge 0 ]; then
+
+  # removing and copy entire directories
 #  [ -d $DIR0003 ] && rm -r $DIR0003
 #  [ -d $DIR0304 ] && rm -r $DIR0304
 
 #  cp -r $DIR0003_org $DIR0003
 #  cp -r $DIR0304_org $DIR0304
+  if [[ "${processd[@]}" =~ "query" ]]; then
+    [ -d $DIR0003/Topics ] && rm -r $DIR0003/Topics
+    cp -r $DIR0003_org/Topics $DIR0003
+    
+    tfil=$DIR0003/Topics/topics2000
+    mv $tfil/TOP-G.txt $tfil/Top-de00.txt
+    mv $tfil/TOP-E.txt $tfil/Top-en00.txt
+    mv $tfil/TOP-SP.txt $tfil/Top-es00.txt
+    mv $tfil/TOP-FI.txt $tfil/Top-fi00.txt
+    mv $tfil/TOP-D.txt $tfil/Top-nl00.txt
+    mv $tfil/TOP-F.txt $tfil/Top-fr00.txt
+    mv $tfil/TOP-I.txt $tfil/Top-it00.txt
+    mv $tfil/TOP-SW.txt $tfil/Top-sw00.txt
 
-#fi
+  fi
+
+  if [[ "${processd[@]}" =~ "rel" ]]; then
+    [ -d $DIR0003/RelAssess ] && rm -r $DIR0003/RelAssess
+    cp -r $DIR0003_org/RelAssess $DIR0003
+
+    mv $RELS/2000rels/biling_qrels $RELS/2000rels/english_qrels
+    mv $RELS/2001rels/qrels_bilingual $RELS/2001rels/qrels_english
+  fi
+
+fi
 
 for lang in "${!L[@]}"; do
 
@@ -64,29 +82,27 @@ for lang in "${!L[@]}"; do
   if [ $sstage -le 0 ] && [ $estage -ge 0 ]; then
 
     if [[ "${processd[@]}" =~ "rel" ]]; then
-      rm $RELS/all_yrs/qrels_${lang}.txt
+      relf=$RELS/all_yrs/qrels_${lang}.txt
+      [[ -f $relf ]] && rm $relf
       mkdir -p $RELS/all_yrs
+    
+      # bear with the ugliness cos formatting is messed up
+      cat $RELS/2000rels/${lang}_qrels >> $relf
+      cat $RELS/2001rels/qrels_${lang} >> $relf
+      lang2=`conv_lang $lang`
+      cat $RELS/2002rels/qrels_${lang2} >> $relf
+      cat $RELS/2003rels/qrels_${lang2} >> $relf
 
-      if [ "$lang" == "english" ]; then
-        cat $RELS/2000rels/biling_qrels >> $RELS/all_yrs/qrels_english.txt
-        cat $RELS/2001rels/qrels_bilingual >> $RELS/all_yrs/qrels_english.txt
-        cat $RELS/2002rels/qrels_EN >> $RELS/all_yrs/qrels_english.txt
-        cat $RELS/2003rels/qrels_EN >> $RELS/all_yrs/qrels_english.txt
-      else 
-        echo $lang
-        cat $RELS/2000rels/${lang}_qrels >> $RELS/all_yrs/qrels_${lang}.txt
-        cat $RELS/2001rels/qrels_${lang} >> $RELS/all_yrs/qrels_${lang}.txt
-        lang2=`conv_lang $lang`
-        cat $RELS/2002rels/qrels_${lang2} >> $RELS/all_yrs/qrels_${lang}.txt
-        cat $RELS/2003rels/qrels_${lang2} >> $RELS/all_yrs/qrels_${lang}.txt
-      fi
-      awk '{ if ($4==1) {print }}' $RELS/all_yrs/qrels_${lang}.txt \
-      > $RELS/all_yrs/qrels_${lang}.txt.temp 
-      mv $RELS/all_yrs/qrels_${lang}.txt.temp $RELS/all_yrs/qrels_${lang}.txt
+      awk '{ if ($4==1) {print }}' $relf > $relf.temp
+      awk '{ if (length($1)==2) {print "query0"$1" "$3} else {print "query"$1" "$3}}' $relf.temp > $relf
+      rm $relf.temp
+      # trec eval format
+      awk '{print $1"\tQ0\t"$2"\t1"}' $relf > $relf.trec
     fi
 
-    # test this function tomorrow
     if [[ "${processd[@]}" =~ "doc" ]]; then
+      rm -r ${L[$lang]}
+      cp -r ${L_org[$lang]} $DOCS
 
       relfile=$RELS/all_yrs/qrels_${lang}.txt
 
@@ -108,25 +124,26 @@ for lang in "${!L[@]}"; do
         mv $DOCS/French_data/new_docno/lemonde.dtd.sgml $DOCS/French_data/new_docno/lemonde.dtd
       fi
 
+      if [ "$lang" == "russian" ]; then
+        rename '.xml' '.sgml' $DOCS/Russian_data/xml/*
+        #ls $DOCS/Russian_data/xml/*.xml | xargs -n1 -I % mv % %.sgml
+      fi
+
       # Extracting documents from sgml files
       for subdir in `ls -I 'valid_ids' -I 'README' -I '*.tgz' -I '*.dtd' -I '*_txt' ${L[$lang]}`; do
         rm_mk ${L[$lang]}/${subdir}_txt
         for fil in `ls ${L[$lang]}/${subdir}/*.sgml`; do
-        
           # check if utf-8, else convert
-          encoding=`file -b --mime-encoding $fil`
-          echo "$encoding > utf-8 $fil"
-
-          if [[ $encoding != "UTF-8" ]]; then
-            if [[ $encoding == "us-ascii" ]]; then
-              encoding="US-ASCII"
-            fi
-            iconv -f ${encoding} -t UTF-8 $fil > ${fil}.t
-            mv ${fil}.t $fil
-          fi
-          # extract documents which are relevant
-          python src/docparser.py ${fil} ${L[$lang]}/${subdir}_txt $relfile
+          conv_encoding $fil
+          # extract documents which are relevant, xclude if they dont exist in relfile 
+          python src/docparser.py doc ${fil} ${L[$lang]}/${subdir}_txt $relfile
         done
+      done
+
+      # Copy all documents into specific all_docs folder
+      rm_mk ${L[$lang]}/all_docs
+      for subdir in `ls -d ${L[$lang]}/*_txt`; do
+        cp $subdir/* ${L[$lang]}/all_docs
       done
 
       # Getting Valid Ids from extracted documents
@@ -138,55 +155,48 @@ for lang in "${!L[@]}"; do
       mv ${L[$lang]}/valid_ids.tmp ${L[$lang]}/valid_ids
     fi # end process doc
 
-    if [[ "${processd[@]}" =~ "mapping" ]]; then
-      savefn=$RELS/all_yrs/qrels_en-$lang.txt
-      python src/merge_clef_relfiles.py $lang $savefn
+    # Deprecated
+    #if [[ "${processd[@]}" =~ "mapping" ]]; then
+    #  savefn=$RELS/all_yrs/qrels_en-$lang.txt
+    #  python src/merge_clef_relfiles.py $lang $savefn
+    #fi
+
+    if [[ "${processd[@]}" =~ "query" ]]; then
+      rm_mk $QUERIES/QUERY_${lang}
+      for yr in 00 01 02 03; do #2000 is broken
+        langt=`conv_lang $lang lower`
+        fil=$QUERIES/topics20${yr}/Top-${langt}${yr}.txt
+        conv_encoding $fil
+
+      #  extract query from sgml file into single line, same as MATERIAL
+        python src/docparser.py query $fil $QUERIES/QUERY_${lang} $yr
+      done
+      
+      cat $QUERIES/QUERY_${lang}/query{00,01,02,03}.txt > $QUERIES/QUERY_${lang}/query.txt
     fi
-
-
   fi # end stage 0
 
   ####### STAGE 1
+  # this script is from print_utils.sh
 
   if [ $sstage -le 1 ] && [ $estage -ge 1 ]; then
     printf "\n$lang - STAGE1: Calculating Statistics:\n"
 
-    #if [[ "${processd[@]}" =~ "doc" ]]; then
-      # TODO
-      # print the directory
-      # num docs
-      # avg nwords per doc
-    #fi
+    if [[ "${processd[@]}" =~ "doc" ]]; then
+      doc_stats ${L[$lang]}/all_docs
+    fi
 
-    if [[ "${processd[@]}" =~ "mapping" ]]; then
+    if [[ "${processd[@]}" =~ "query" ]]; then
+      print_query $QUERIES/QUERY_${lang}/query.txt
+    fi
 
-      # this script mirrors run_all.sh
-
-      if [[ "$lang" != "english" ]]; then
-
-        printf "\n=== mapping ===\n"
-        v1=$(wc -l $RELS/all_yrs/qrels_en-$lang.txt | awk '{print $1}')
-
-        numq=$(awk '{print $1}' $RELS/all_yrs/qrels_en-$lang.txt | sort -u | wc -l)
-        numd=$(awk '{print $2}' $RELS/all_yrs/qrels_en-$lang.txt | sort -u | wc -l)
-
-        v3=$(awk "BEGIN{print $v1/$numq}")
-        v4=$(awk "BEGIN{print $v1/$numd}")
-
-        printf "\tnum of unique english docs: $numq\n"
-        printf "\tnum of unique $lang docs: $numd\n"
-
-        printf "\tavg num of $lang docs to english docs: $v3\n"
-        printf "\tavg num of english docs to $lang doc: $v4\n"
-
-        # trec eval format
-        awk '{print $1"\tQ0\t"$2"\t1"}' $RELS/all_yrs/qrels_en-$lang.txt \
-        > $RELS/all_yrs/qrels_en-$lang.txt.trec
-      fi
+    if [[ "${processd[@]}" =~ "rel" ]]; then
+      print_mapping $RELS/all_yrs/qrels_$lang.txt
     fi
     
   fi
 #  if [ $sstage -le 2 ] && [ $estage -ge 2 ]; then
+#  fi
 #  if [ $sstage -le 3 ] && [ $estage -ge 3 ]; then
 #  if [ $sstage -le 4 ] && [ $estage -ge 4 ]; then
 #  if [ $sstage -le 5 ] && [ $estage -ge 5 ]; then
---
+++ bin/utils.sh
@@ -0,0 +1,112 @@
+#!/usr/bin/env bash
+# Author: Suzanna Sia
+
+function rm_mk(){
+  [ -d $1 ] && rm -r $1
+   mkdir -p $1
+}
+
+
+function print_mapping(){
+  printf "\n=== mapping ===\n"
+  mapf=$1
+
+  v1=$(wc -l $mapf | awk '{print $1}')
+
+  numq=$(awk '{print $1}' $mapf | sort -u | wc -l)
+  numd=$(awk '{print $2}' $mapf | sort -u | wc -l)
+
+  v3=$(awk "BEGIN{print $v1/$numq}")
+  v4=$(awk "BEGIN{print $v1/$numd}")
+
+  printf "\tnum of unique queries: $numq\n"
+  printf "\tnum of unique docs: $numd\n"
+
+  printf "\tavg num of docs per query: $v3\n"
+  printf "\tavg num of query per doc: $v4\n"
+}
+
+function print_query(){
+  queryf=$1
+
+  ntotalq=$(cat $queryf | sort -u | wc -l)
+  ntotalqtext=$(cat $queryf | awk -F'\t' '{print $2}' | sort -u | wc -l)
+  avgwords_q=$(cat $queryf | awk -F'\t' '{print $2}' | wc | awk '{print $2/$1}')
+
+  printf "\n=== Queries === \n"
+  printf "\tno. of unique $lang queryIDs: $ntotalq, unique text: $ntotalqtext \n"
+  printf "\taverage no. of words: $avgwords_q\n"
+
+}
+
+function doc_stats() {
+    printf "\n=== Docs ===\n"
+    printf "from: $1\n"
+    
+    #eng_nw=$(cat $TEMP_DIR/DOCS_$lang/$1/$2/* | wc -w)
+    #src_nw=$(cat $TEMP_DIR/DOCS_$lang/$1/src/* | wc -w)
+    #nlines=$(cat $TEMP_DIR/DOCS_$lang/$1/$2/* | wc -l)
+    #ndocs=$(ls $TEMP_DIR/DOCS_$lang/$1/$2/* | wc -l)
+
+    nw=$(cat $1/* | wc -w)
+    nlines=$(cat $1/* | wc -l)
+    ndocs=$(ls $1/* | wc -l)
+
+    avg_nw_doc=$(awk "BEGIN{print $nw/$ndocs}")
+    #src_avg_nw_doc=$(awk "BEGIN{print $src_nw/$ndocs}")
+    if [ $nlines -eq 0 ]; then
+      echo "no line info (joined doc not for MT)"
+    else
+      avg_nw_lin=$(awk "BEGIN{print $nw/$nlines}")
+      avg_nlines=$(awk "BEGIN{print $nlines/$ndocs}")
+      printf "\tnum lines: $nlines\n" 
+      printf "\tavg nwords per line: $avg_nw_lin \n"
+      printf "\tavg num lines per doc: $avg_nlines\n"
+    fi
+    #src_avg_nw_lin=$(awk "BEGIN{print $src_nw/$nlines}")
+
+    printf "\tnum docs: $ndocs\n"
+    #printf "\tavg nwords per line for src: $src_avg_nw_lin \n"
+    printf "\tavg nwords per doc: $avg_nw_doc \n"
+    #printf "\tavg nwords per doc for src: $src_avg_nw_doc \n"
+}
+
+
+
+conv_lang() {
+  [[ "$1" == "english" ]] && lang="EN";
+  [[ "$1" == "spanish" ]] && lang="ES";
+  [[ "$1" == "german" ]] && lang="DE";
+  [[ "$1" == "french" ]] && lang="FR";
+  [[ "$1" == "finnish" ]] && lang="FI";
+  [[ "$1" == "italian" ]] && lang="IT";
+  [[ "$1" == "russian" ]] && lang="RU";
+  [[ "$1" == "swedish" ]] && lang="SV";
+
+  [[ "$2" == "lower" ]] && lang=`echo $lang | awk '{print tolower($0)}'`
+  echo "$lang"
+}
+
+conv_encoding() {
+  # check if utf-8, else convert
+  fil=$1
+  encoding=`file -b --mime-encoding $fil`
+
+  if [[ $encoding != "utf-8" ]]; then
+    [[ $encoding == "us-ascii" ]] && encoding="US-ASCII"
+    [[ $encoding == "utf-16le" ]] && encoding="UTF-16LE"
+
+    echo "$encoding > utf-8 $fil"
+    iconv -f ${encoding} -t UTF-8 $fil > ${fil}.t
+
+    if [ $? -eq 0 ]; then
+      mv ${fil}.t $fil
+    else
+      echo "encoding:$encoding, don't do anythng with $fil"
+      rm ${fil}.t
+    fi
+  else
+    echo "utf-8 > utf-8 $fil"
+  fi
+} 2>encoding.err
+

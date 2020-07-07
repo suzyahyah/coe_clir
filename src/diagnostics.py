9b637887a6417d1992a0c1d8802324a8014125fb
suzyahyah <suzyahyah@gmail.com>
2020-07-07 08:08:55 -0400

    remove useless imports

--- src/diagnostics.py
+++ src/diagnostics.py
@@ -1,26 +1,11 @@
 #!/usr/bin/python3
 # Author: Suzanna Sia
 
-### Standard imports
-#import random
 import numpy as np
 import pdb
-#import math
 import os
 import sys
-#import argparse
 
-### Third Party imports
-
-### Local/Custom imports
-
-# Comment out debugger before deployment
-from debugger import Debugger
-DB = Debugger()
-DB.debug_mode=False
-#from distutils.util import str2bool
-#argparser = argparser.ArgumentParser()
-#argparser.add_argument('--x', type=float, default=0)
 
 def vocab_overlap(vocab1, vocab2):
 
